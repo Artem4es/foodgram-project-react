8a2de27420933530d146362e90cb098696a6f57c
Артём Ежов <dragster1001@yandex.ru>
2023-05-30 11:42:57 +0300

    change PAgeLimitPagination

--- backend/food/api/v1/custom_pagination.py
+++ backend/food/api/v1/custom_pagination.py
@@ -8,19 +8,4 @@ DEFAULT_PAGE_SIZE = api_settings.PAGE_SIZE
 class PageLimitPagination(PageNumberPagination):
     """Only page and limit parameters"""
 
-    if not isinstance(DEFAULT_PAGE_SIZE, int):
-        raise Exception(
-            '"PAGE_SIZE" in settings.py should be a valid integer and > 0'
-        )
-    if DEFAULT_PAGE_SIZE < 1:
-        raise Exception('"PAGE_SIZE" in settings.py can\'t be less than 1')
-
-    def get_page_size(self, request):
-        page_size = request.query_params.get('limit', DEFAULT_PAGE_SIZE)
-        try:
-            page_size = int(page_size)
-        except ValueError:
-            raise ValidationError('"limit" должен быть числом!')
-        if page_size < 1:
-            raise ValidationError('"limit" не может быть меньше 1')
-        return page_size
+    page_size_query_param = 'limit'
